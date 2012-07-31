5966cc9afe15bb18aed08e37adf135f92d98e003
gka <gka@vis4.net>
2012-08-01 01:04:38 +0200

    initial commit

---
+++ apps.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import web
+
+urls = (
+  '/', 'hello',
+  '/([^\/]+)', 'app_index',
+  '/([^\/]+)/newtask', 'app_newtask',
+  '/([^\/]+)/task/(\d+)', 'app_task'
+)
+
+app = web.application(urls, globals())
+render = web.template.render('templates/')
+
+req_count = 0
+
+class hello:
+    def GET(self):
+	global req_count
+	req_count += 1
+        return 'Hello, offeneparteien! %d' % req_count
+
+class app_index:
+    def GET(self, app_name):
+	return render.app(app_name)
+
+class app_task:
+    def GET(self, app_name, task_id):
+        return 'solve task '+str(task_id) +' of '+app_name
+
+class app_newtask:
+    def GET(self, app_name):
+        return 'redirect to new task in '+app_name
+
+
+
+
+if __name__ == "__main__":
+#    web.wsgi.runwsgi = lambda func, addr = None: web.wsgi.runfcgi(func, addr) 
+    app.run()
