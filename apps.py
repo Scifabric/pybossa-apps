f3a24a96d18c8cd4eb139d6927d68f8212fdb098
Gregor Aisch <contact@vis4.net>
2012-08-01 02:40:32 +0200

    added templates task and newtask

--- apps.py
+++ apps.py
@@ -14,34 +14,21 @@ urls = (
 app = web.application(urls, globals())
 render = web.template.render('templates/')
 
-req_count = 0
-
-
-class hello:
-    def GET(self):
-        global req_count
-        req_count += 1
-        return 'Hello, offeneparteien! %d' % req_count
-
 
 class app_index:
     def GET(self, app_name):
-        global endpoint
         return render.app(endpoint, app_name)
-
-
-class app_task:
-    def GET(self, app_name, task_id):
-        return 'solve task ' + str(task_id) + ' of ' + app_name
 
 
 class app_newtask:
     def GET(self, app_name):
-        return 'redirect to new task in ' + app_name
+        return render.newtask(endpoint, app_name)
 
 
+class app_task:
+    def GET(self, app_name, task_id):
+        return render.task(endpoint, app_name, task_id)
 
 
 if __name__ == "__main__":
-#    web.wsgi.runwsgi = lambda func, addr = None: web.wsgi.runfcgi(func, addr) 
     app.run()
--- static/js/pybossa-apps.js
+++ static/js/pybossa-apps.js
@@ -1,29 +1,57 @@
 
 (function(global, $) {
 
-    function app() {
+    function _load_app(callback) {
         $.ajax({
             url: global.endpoint + '/api/app?short_name=' + global.app_name,
             success: function(data) {
-                $('.loading').hide();
-                $('.container').show();
-
-                var app = data[0];
-                $('.app-name').html(app.name);
-                $('.app-description').html(app.description);
-                $('.app-long-description').html(app.long_description);
-                $('.solve-tasks').attr('href', '/'+global.app_name+'/newtask');
-                $('.solve-tasks').html(app.info.engage_text ? app.info.engage_text : 'Solve some tasks');
+                callback(data[0]);
             }
         });
     }
 
-    function newtask() {
+    function _load_task(id, callback) {
+        $.ajax({
+            url: global.endpoint + '/api/task/' + id,
+            success: function(task) {
+                callback(task);
+            }
+        });
+    }
 
+    function app() {
+        _load_app(function(app) {
+            $('.loading').hide();
+            $('.container').show();
+
+            $('.app-name').html(app.name);
+            $('.app-description').html(app.description);
+            $('.app-long-description').html(app.long_description);
+            $('.solve-tasks').attr('href', '/'+global.app_name+'/newtask');
+            $('.solve-tasks').html(app.info.engage_text ? app.info.engage_text : 'Do some Tasks!');
+        });
+    }
+
+    function newtask() {
+        pybossa.newTask(global.app_name).done( function( data ) {
+            if ( !$.isEmptyObject(data.task) ) {
+                window.location.pathname = '/'+global.app_name+'/task/' + data.task.id;
+            } else {
+                $("#finish").fadeIn();
+            }
+        });
     }
 
     function task() {
+        // at first we load the task presenter
+        _load_app(function(app) {
+            $('#task-presenter').html(app.info.task_presenter);
 
+            // then we load load the task
+            _load_task(global.task_id, function(task) {
+                taskLoaded(task);
+            });
+        });
     }
 
     $(function() {
