f3416eb6681e322f8c099a177236c25579b4efe2
Gregor Aisch <contact@vis4.net>
2012-08-01 02:22:52 +0200

    template updates

--- static/js/pybossa-apps.js
+++ static/js/pybossa-apps.js
@@ -5,7 +5,15 @@
         $.ajax({
             url: global.endpoint + '/api/app?short_name=' + global.app_name,
             success: function(data) {
-                console.log(data);
+                $('.loading').hide();
+                $('.container').show();
+
+                var app = data[0];
+                $('.app-name').html(app.name);
+                $('.app-description').html(app.description);
+                $('.app-long-description').html(app.long_description);
+                $('.solve-tasks').attr('href', '/'+global.app_name+'/newtask');
+                $('.solve-tasks').html(app.info.engage_text ? app.info.engage_text : 'Solve some tasks');
             }
         });
     }
